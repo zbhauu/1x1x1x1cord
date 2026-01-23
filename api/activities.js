18ca344a818a2711e3afc09ec10b8fe114ece0fc
zbhauu <itsallfineiguess@gmail.com>
2026-01-23 19:41:19 +0200

    Delete api directory

--- api/activities.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router({ mergeParams: true });
-
-router.get("/statistics/applications/:applicationid", async (req, res) => {
-    return res.status(200).json([]);
-});
-
-module.exports = router;
